e01babf6122c00f56549f5d037e834761d9f9fda
Dani Pozo <danipozo@correo.ugr.es>
2021-01-19 13:16:16 +0100

    Divide arguments in groups

--- src/train.py
+++ src/train.py
@@ -9,19 +9,34 @@ Main script used for training the model.
 '''
 
 parser = argparse.ArgumentParser(
-    help='Main script used to train the model.'
+    description='Main script used to train the model.'
+)
+
+data_loading = parser.add_argument_group(
+    'Data loading',
+    'Data loading arguments'
+)
+
+model_group = parser.add_argument_group(
+    'Model',
+    'Model arguments'
+)
+
+training_group = parser.add_argument_group(
+    'Training',
+    'Training arguments'
 )
 
 # DATA LOADING ARGUMENTS
-parser.add_argument(
-    name='--dataset',
+data_loading.add_argument(
+    '--dataset',
     choices=['toy_50k', 'im2latex'],
     required=True,
     help='Select the dataset used for training'
 )
 
-parser.add_argument(
-    name='--samples',
+data_loading.add_argument(
+    '--samples',
     type=int,
     required=True,
     help='Number of samples to train on'
@@ -30,88 +45,90 @@ parser.add_argument(
 # MODEL ARGUMENTS
 
 # Transformer arguments
-parser.add_argument(
-    name='--num-layers',
+model_group.add_argument(
+    '--num-layers',
     type=int,
     required=True,
     help='Number of layers of the encoder and decoder'
 )
 
-parser.add_argument(
-    name='--depth',
+model_group.add_argument(
+    '--depth',
     type=int,
     required=True,
     help='Depth parameter of the model'
 )
 
-parser.add_argument(
-    name='--feedforward-units',
+model_group.add_argument(
+    '--feedforward-units',
     type=int,
     required=True,
     help='Units on pointwise feedforward network'
 )
 
-parser.add_argument(
-    name='--num-heads',
+model_group.add_argument(
+    '--num-heads',
     type=int,
     required=True,
     help='Number of heads on the multihead attention'
 )
 
-parser.add_argument(
-    name='--dropout-rate',
+model_group.add_argument(
+    '--dropout-rate',
     type=float,
     required=True,
     help='Dropout rate in the transformer'
 )
 
-parser.add_argument(
-    name='--performer-attention-encoder',
-    choices=['yes', 'no']
+model_group.add_argument(
+    '--performer-attention-encoder',
+    choices=['yes', 'no'],
     required=True,
     help='Whether to employ the FAVOR+ mechanism for attention in the encoder'
 )
 
-parser.add_argument(
-    name='--positional-encoding',
-    choices=['standard', '2d']
+model_group.add_argument(
+    '--positional-encoding',
+    choices=['standard', '2d'],
     required=True,
     help='Positional encoding type'
 )
 
 # Convolutional encoder arguments
-parser.add_argument(
-    name='--conv-encoder',
-    choices=['vanilla', 'resnet']
+model_group.add_argument(
+    '--conv-encoder',
+    choices=['vanilla', 'resnet'],
     required=True,
     help='Type of convolutional encoder to employ'
 )
 
-parser.add_argument(
-    name='--pretrain-conv-encoder',
-    choices=['yes', 'no']
+model_group.add_argument(
+    '--pretrain-conv-encoder',
+    choices=['yes', 'no'],
     required=True,
     help='Whether to pretrain the convolutional encoder'
 )
 
 # TRAINING PARAMETERS
-parser.add_argument(
-    name='--epochs',
+training_group.add_argument(
+    '--epochs',
     type=int,
     required=True,
     help='Number of epochs to train'
 )
 
-parser.add_argument(
-    name='--optimizer',
-    choices=['adam', 'sgd', 'padam']
+training_group.add_argument(
+    '--optimizer',
+    choices=['adam', 'sgd', 'padam'],
     required=True,
     help='Optimizer to employ'
 )
 
-parser.add_argument(
-    name='--lr-schedule',
-    choices=['constant', 'cyclical']
+training_group.add_argument(
+    '--lr-schedule',
+    choices=['constant', 'cyclical'],
     required=True,
     help='Learning rate schedule'
 )
+
+args = parser.parse_args()
