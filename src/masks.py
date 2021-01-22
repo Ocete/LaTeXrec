ad5d1dde20da536f11e4ad0e94cc5ffed098dd49
Dani Pozo <danipozo@correo.ugr.es>
2021-01-22 13:09:26 +0100

    Add missing file with masks code

---
+++ src/masks.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+'''
+This file contains the code for the sequence masks.
+'''
+
+def create_look_ahead_mask(size):
+    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+    return mask  # (seq_len, seq_len)
